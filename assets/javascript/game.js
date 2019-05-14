1e51e85ec152244eae62bb7a25ebc7ab941d42fd
Louis Rollins <louisrollins01@gmail.com>
2019-05-14 18:29:57 -0400

    edited fight button to make new hp for player. appears on console log but not on page

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,44 +3,52 @@ var player = "";
 var enemy = "";
 
 //for each character clicked on..
-$character.each(function(index, character) {
+$character.each(function (index, character) {
     var $element = $(character)
-    $element.on('click', function() {
-        
+    $element.on('click', function () {
+
         //move all opponents not clicked on under opponent
         var $opponents = $character.not(($(this)))
         $opponents.insertAfter($("#opponent"))
-        var player = $(this)
+        player = $(this)
         $element.off('click')
 
-        $opponents.each(function(index, opponent) {
+        $opponents.each(function (index, opponent) {
             var $opponent = $(opponent)
             $opponent.off('click')
 
         })
 
-        //hide unchosen fighters and move dueling fighters under match
-        $opponents.each(function(index, opponent) {
-            var $opponent = $(opponent)
-            $opponent.on('click', function(){
-                var $hidden = $opponents.not(($(this)))
-              $hidden.hide()
 
-            var enemy = ($(this))
-              player.insertBefore($('#versus'))
+        //hide unchosen fighters and move dueling fighters under match
+        $opponents.each(function (index, opponent) {
+            var $opponent = $(opponent)
+            $opponent.on('click', function () {
+                var $hidden = $opponents.not(($(this)))
+                $hidden.hide()
+
+                enemy = ($(this))
+                player.insertBefore($('#versus'))
                 enemy.insertAfter($('#versus'))
-        
+
             })
+
+
         })
-      
+        var $fightButton = $("#fight-button")
+        $fightButton.on('click', function () {
+            $hp = $(".hp")
+            console.log(player)
+            console.log(enemy);
+            var playerHp = $(player).find("p").text()
+            var newHp = parseInt(playerHp, 10) + 500 ;
+            
+            console.log(newHp)
+            $(playerHp).append($(newHp).text())
         })
-    
-    var $fightButton = $("#fight-button")
-    
-    $fightButton.on('click', function(){
-        $hp = $(".hp")
-        $hp++
+
 
     })
-})
 
+
+})
